5ffa1ea77dd87005223eb38ed5659d0a38fec53e
Romila Pradhan <rpradhan@purdue.edu>
2015-01-15 20:53:14 -0500

    updated dataStats.java and generator.java
    dataStats can now pull all these information from real data:
     - degree of objects
     - gini values of objects
     - entropies of objects
     - conflict ratio of objects = for each object, ratio of #votes for two leading values, e.g, 5 sources vote for value 0, 10 sources vote for value 1, 3 vote for value 2, then conflict ratio = 10/5 = 2.
     - fraction of sources voting for each object
    
    generator can now generate these kinds of synthetic data:
     - completely random
     - following degree distribution of real data

--- dataGenerator/dataStats.java
+++ dataGenerator/dataStats.java
@@ -2,8 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class dataStats {
+	public List<List<String>> dataTuples;
+	public int numSources;
+	public int numObjects;
+	public List<List<String>> objectValues;
+	public int[] numberSourceVoted;
 	
-	// read data file into tuples
+	public dataStats(String dataFile) throws IOException {
+		this.dataTuples   = getTuplesFromFile (dataFile);
+	
+		this.numObjects   = this.dataTuples.size();
+		this.numSources   = getNumberOfSources(this.dataTuples);
+		this.numberSourceVoted = new int[this.numSources];
+		getObjectUniqueValues();
+	}
+	
+	/*
+	 * function to read data file into tuples
+	 */
 	public List<List<String>> getTuplesFromFile(String fileName) throws IOException {
 		BufferedReader reader = new BufferedReader(new FileReader(fileName));
 		
@@ -25,177 +41,203 @@ public class dataStats {
         return fileTuples;
 	}
 	
-	// write results to file
-	public  void writeToFile(List<String> results, String fileName) throws IOException {
+	/*
+	 * function to return number of sources from data tuples
+	 */
+	public int getNumberOfSources(List<List<String>> dataTuples) {
+		List<Integer> numSourcesObjects = new ArrayList<Integer>();
+		for (int i = 0; i < this.numObjects; i++)
+			numSourcesObjects.add(dataTuples.get(i).size());
+		return Collections.max(numSourcesObjects);
+	}
+	
+	/*
+	 * function to return unique object values for a tuple
+	 */
+	public void getObjectUniqueValues() {
+		this.objectValues = new ArrayList<List<String>>();
+		for (int i = 0; i < this.numObjects; i++) {
+			ArrayList<String> objectUniqueValues = new ArrayList<String>();
+			for (int j = 0; j < this.dataTuples.get(i).size(); j++) {
+				if (this.dataTuples.get(i).get(j) != null) {
+					objectUniqueValues.add(this.dataTuples.get(i).get(j));
+					this.numberSourceVoted[j]++;
+				}
+			}
+			objectUniqueValues = new ArrayList<String>(new HashSet<String>(objectUniqueValues));
+			this.objectValues.add(objectUniqueValues);
+		}
+	}
+	
+	/*
+	 * function to write results to file
+	 */
+	public  void writeToFile(double[] results, String fileName) throws IOException {
 		BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
 		
-		for ( int i = 0; i < results.size(); i++)
-	        writer.write(results.get(i)+ "\n");
+		for ( int i = 0; i < results.length; i++)
+	        writer.write(results[i] + "\n");
 		
 		writer.close();
 	}
 
-	// get degree of each object
-	public int getDegreeOfObject(List<String> tupleData) {
-		int count = 0;
-		for (int i = 0; i < tupleData.size(); i++) 
-			if (tupleData.get(i) != null)
-				count++;
-		
+	/*
+	 * function to get degree of each object
+	 */
+	public double[] getDegreeOfObjects() {
+		double[] count = new double[this.dataTuples.size()];
+		for (int i = 0; i < this.dataTuples.size(); i++) {
+			for (int j = 0; j < this.dataTuples.get(i).size(); j++) {
+				if (this.dataTuples.get(i).get(j) != null)
+					count[i]++;
+			}
+		}
 		return count;
 	}
 	
-	/* get agreement/disagreement for each object,
+	/* 
+	 * function to get agreement/disagreement for each object,
 	 * levelOfConflict .. ratio of conflict in top contesting values
 	 */
-	public double getConflictRatio(List<String> tupleData) {
-		if (tupleData.size() == 0)
-			return 0;
-		
-		ArrayList<String> objectUniqueValues = new ArrayList<String>(new HashSet<String>(tupleData));
-		objectUniqueValues.removeAll(Collections.singleton(null));
-		
-		String[][] valuesCount = new String[objectUniqueValues.size()][2]; // value in [0], count in [1]
-		for (int i = 0; i < valuesCount.length; i++) {
-			valuesCount[i][0] = objectUniqueValues.get(i);
-			valuesCount[i][1] = String.valueOf(0);
-		}
-		
-		for (int i = 0; i < tupleData.size(); i++) {
-			if (tupleData.get(i) != null) {
-				for (int j = 0; j < valuesCount.length; j++) {
-					if (valuesCount[j][0].equals(tupleData.get(i))) {
-						valuesCount[j][1] = String.valueOf(Integer.parseInt(valuesCount[j][1]) + 1);
-						break;
+	public double[] getConflictRatios() {
+		double[] conflictRatio = new double[this.dataTuples.size()];
+		for (int i = 0; i < this.dataTuples.size(); i++) {
+			List<String> objectValues = this.objectValues.get(i);
+			objectValues.removeAll(Collections.singleton(null));
+			
+			String[][] valuesCount = new String[objectValues.size()][2]; // value in [0], count in [1]
+			for (int j = 0; j < valuesCount.length; j++) {
+				valuesCount[j][0] = objectValues.get(j);
+				valuesCount[j][1] = String.valueOf(0);
+			}
+			
+			for (int j = 0; j < this.dataTuples.get(i).size(); j++) {
+				if (this.dataTuples.get(i).get(j) != null) {
+					for (int k = 0; k < valuesCount.length; k++) {
+						if (valuesCount[k][0].equals(this.dataTuples.get(i).get(j))) {
+							valuesCount[k][1] = String.valueOf(Integer.parseInt(valuesCount[k][1]) + 1);
+							break;
+						}
 					}
 				}
 			}
-		}
-		
-		int firstMax = 0; int index = 0;
-		int secondMax = 0;
-		
-		for (int i = 0; i < valuesCount.length; i++) {
-			if (Integer.parseInt(valuesCount[i][1]) > firstMax) {
-				firstMax = Integer.parseInt(valuesCount[i][1]);
-				index = i;
+			
+			if (objectValues.size() > 0) {
+				int firstMax = 0; int index = 0;
+				int secondMax = 0;
+				
+				for (int j = 0; j < valuesCount.length; j++) {
+					if (Integer.parseInt(valuesCount[j][1]) > firstMax) {
+						firstMax = Integer.parseInt(valuesCount[j][1]);
+						index = j;
+					}
+				}
+				
+				valuesCount[index][1] = String.valueOf(0); index = 0;
+				for (int j = 0; j < valuesCount.length; j++) {
+					if (Integer.parseInt(valuesCount[j][1]) > secondMax) {
+						secondMax = Integer.parseInt(valuesCount[j][1]);
+						index = j;
+					}
+				}
+				
+				if (secondMax != 0)
+					conflictRatio[i] = (double) firstMax/secondMax;
 			}
 		}
-		
-		valuesCount[index][1] = String.valueOf(0); index = 0;
-		for (int i = 0; i < valuesCount.length; i++) {
-			if (Integer.parseInt(valuesCount[i][1]) > secondMax) {
-				secondMax = Integer.parseInt(valuesCount[i][1]);
-				index = i;
-			}
-		}
-		
-		double conflictRatio = 0;
-		if (secondMax != 0)
-			conflictRatio = (double) firstMax/secondMax;
-		System.out.println(conflictRatio);
 		
 		return conflictRatio;
-		
-		
 	}
 	
-	// get entropy of each object
-	public double getEntropyOfObject(List<String> tupleData) {
-		double entropy = 0;
-		int count = 0;
-		for (int i = 0; i < tupleData.size(); i++) 
-			if (tupleData.get(i) != null)
-				count++;
+	/*
+	 * function to get entropy for each object
+	 */
+	public double[] getEntropyOfObjects() {
+		double[] entropy = new double[this.dataTuples.size()];
 		
-		ArrayList<String> objectUniqueValues = new ArrayList<String>(new HashSet<String>(tupleData));
-		objectUniqueValues.removeAll(Collections.singleton(null));
-		
-		if (objectUniqueValues.size() > 0) {
-			double[] fractionUnique = new double[objectUniqueValues.size()];
-			for (int i = 0; i < objectUniqueValues.size(); i++) {
-//				System.out.println(Collections.frequency(tupleData, objectUniqueValues.get(i)));
-				fractionUnique[i] = (double) Collections.frequency(tupleData, objectUniqueValues.get(i))/count;
-				if (fractionUnique[i] > 0)
-					entropy += -1 * fractionUnique[i] * Math.log(fractionUnique[i]);
+		for (int i = 0; i < this.dataTuples.size(); i++) {
+			int count = 0;
+			for (int j = 0; j < this.dataTuples.get(i).size(); j++)
+				if (this.dataTuples.get(i).get(j) != null)
+					count++;
+			
+			List<String> objectValues = this.objectValues.get(i);
+			objectValues.removeAll(Collections.singleton(null));
+			
+			if (objectValues.size() > 0) {
+				double[] fraction = new double[objectValues.size()];
+				for (int j = 0; j < objectValues.size(); j++) {
+					fraction[j] = (double) Collections.frequency(this.dataTuples.get(i), objectValues.get(j)) / count;
+					if (fraction[j] > 0)
+						entropy[i] += -1 * fraction[j] * Math.log(fraction[j]); 
+				}
 			}
 		}
 		
 		return entropy;
 	}
 	
-	// get entropy of each object
-	public double getGiniValueOfObjects(List<String> tupleData) {
-		double giniValue = 0;
-		int count = 0;
-		for (int i = 0; i < tupleData.size(); i++) 
-			if (tupleData.get(i) != null)
-				count++;
+	/*
+	 * function to get gini value for each object
+	 */
+	public double[] getGiniValueOfObjects() {
+		double[] giniValue = new double[this.dataTuples.size()];
 		
-		ArrayList<String> objectUniqueValues = new ArrayList<String>(new HashSet<String>(tupleData));
-		objectUniqueValues.removeAll(Collections.singleton(null));
-		
-		if (objectUniqueValues.size() > 0) {
-			double[] fractionUnique = new double[objectUniqueValues.size()];
-			for (int i = 0; i < objectUniqueValues.size(); i++) {
-				fractionUnique[i] = (double) Collections.frequency(tupleData, objectUniqueValues.get(i))/count;
-				if (fractionUnique[i] > 0)
-					giniValue += fractionUnique[i] * fractionUnique[i];
+		for (int i = 0; i < this.dataTuples.size(); i++) {
+			int count = 0;
+			for (int j = 0; j < this.dataTuples.get(i).size(); j++)
+				if (this.dataTuples.get(i).get(j) != null)
+					count++;
+			
+			List<String> objectValues = this.objectValues.get(i);
+			objectValues.removeAll(Collections.singleton(null));
+			
+			if (objectValues.size() > 0) {
+				double[] fraction = new double[objectValues.size()];
+				for (int j = 0; j < objectValues.size(); j++) {
+					fraction[j] = (double) Collections.frequency(this.dataTuples.get(i), objectValues.get(j)) / count;
+					if (fraction[j] > 0)
+						giniValue[i] += fraction[j] * fraction[j]; 
+				}
 			}
+			giniValue[i] = 1 - giniValue[i];
 		}
-		
-		return (1 - giniValue);
+		return giniValue;
 	}
 	
-	// args[0]: filename
-	public static void main(String[] args) {
-		dataStats d = new dataStats();
-		List<List<String>>  dataTuples = new ArrayList<List<String>>();
-		try {
-			dataTuples = d.getTuplesFromFile (args[0]);
+	/*
+	 * function to get % of sources that vote for an object
+	 */
+	public double[] getFractionOfSourcesVotingForObject() {
+		double[] fractionVoted = new double[this.dataTuples.size()];
+		for (int i = 0; i < this.dataTuples.size(); i++) {
+			for (int j = 0; j < this.dataTuples.get(i).size(); j++) {
+				if (this.dataTuples.get(i).get(j) != null)
+					fractionVoted[i]++;
+			}
+			fractionVoted[i] /= this.numSources;
 		}
-		catch (Exception e) {
-			System.out.println("File reading error");
-		}
+		return fractionVoted;
+	}
+	
+	/*
+	 *  args[0]: filename that needs to be analyzed
+	 */
+	public static void main(String[] args) throws IOException {
+		dataStats d = new dataStats(args[0]);
 		
-		List<String> degreeOfObjects = new ArrayList<String>();
-		for (int i = 0; i < dataTuples.size(); i++)
-			degreeOfObjects.add(String.valueOf(d.getDegreeOfObject(dataTuples.get(i))));
-
-		try {
-			d.writeToFile(degreeOfObjects, "degreeOfObjects.txt");
-		} catch (Exception e) {
-			System.out.println("File writing error");
-		}
+		double[] degreeOfObjects = d.getDegreeOfObjects();
+		double[] conflictOfObjects = d.getConflictRatios();		
+		double[] entropyOfObjects = d.getEntropyOfObjects();
+		double[] giniValueOfObjects = d.getGiniValueOfObjects();
+		double[] fractionOfSourcesVotingForObjects = d.getFractionOfSourcesVotingForObject();
 		
-//		List<Double> conflictOfObjects = new ArrayList<Double>();
-//		for (int i = 0; i < dataTuples.size(); i++)
-//			conflictOfObjects.add(d.getConflictRatio(dataTuples.get(i)));
-//
-//		try {
-//			d.writeToFile(conflictOfObjects, "conflictOfObjects.txt");
-//		} catch (Exception e) {
-//			System.out.println("File writing error");
-//		}
-
-//		List<Double> entropyOfObjects = new ArrayList<Double>();
-//		for (int i = 0; i < dataTuples.size(); i++)
-//			entropyOfObjects.add(d.getEntropyOfObject(dataTuples.get(i)));
-//
-//		try {
-//			d.writeToFile(entropyOfObjects, "entropyOfObjects.txt");
-//		} catch (Exception e) {
-//			System.out.println("File writing error");
-//		}
+		d.writeToFile(degreeOfObjects, "degree.txt");
+		d.writeToFile(conflictOfObjects, "conflictRatio.txt");
+		d.writeToFile(entropyOfObjects, "entropy.txt");
+		d.writeToFile(giniValueOfObjects, "giniValue.txt");
+		d.writeToFile(fractionOfSourcesVotingForObjects, "fractionVoting.txt");
 		
-		List<String> giniValueOfObjects = new ArrayList<String>();
-		for (int i = 0; i < dataTuples.size(); i++)
-			giniValueOfObjects.add(String.valueOf(d.getGiniValueOfObjects(dataTuples.get(i))));
-
-		try {
-			d.writeToFile(giniValueOfObjects, "giniValueOfObjects.txt");
-		} catch (Exception e) {
-			System.out.println("File writing error");
-		}
+		System.out.println("End of program");
 	}
 }
--- dataGenerator/generator.java
+++ dataGenerator/generator.java
@@ -3,22 +3,31 @@ import java.util.*;
 
 public class generator {
 	public String degreeDistributionFile;
-	public String entropyDistributionFile;
-	public int numObjects;
-	public int numSources;
+	public String giniDistributionFile;
+	public String fractionVotingFile;
+	public String conflictRatioFile;
+	
+	public int numObjects; // #objects in synthetic data
+	public int numSources; // #sources in synthetic data
 	public String data[][];
 	public String truth[][];
-	public Random random; 
 		
-	public generator (int nObjects, int nSources) {
-		this.numObjects = nObjects;
-		this.numSources = nSources;
-		this.random = new Random(System.currentTimeMillis());
+	public generator(String[] args) {
+		this.numObjects = Integer.parseInt(args[0]);
+		this.numSources = Integer.parseInt(args[1]);
+		
+		this.degreeDistributionFile = args[2];
+		this.giniDistributionFile = args[3]; 
+		this.fractionVotingFile = args[4];
+		this.conflictRatioFile = args[5];
+		
 		this.data = new String[this.numObjects][this.numSources];
 		this.truth = new String[this.numObjects][2];
 	}
 	
-	// read file
+	/*
+	 * function to read a file
+	 */
 	public List<String> getValuesFromFile(String fileName) throws IOException {
 		BufferedReader reader = new BufferedReader(new FileReader(fileName));
 			
@@ -33,7 +42,9 @@ public class generator {
         return fileTuples;
 	}
 	
-	// write results to file
+	/*
+	 * function to write results to a file
+	 */
 	public  void writeToFile(String[][] results, String fileName) throws IOException {
 		BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
 			
@@ -47,63 +58,6 @@ public class generator {
 		}
 		
 		writer.close();
-	}
-	
-	// generate nObjects with normal distribution
-	public double[] drawFromNormalDistribution(List<String> distributionStrings, int nObjects) {
-		double mean = 0;
-		double stddev = 0;
-		Random random = new Random(System.currentTimeMillis());
-		
-		for (int i = 0; i < distributionStrings.size(); i++) 
-			mean += Double.parseDouble(distributionStrings.get(i));
-		
-		mean /= distributionStrings.size();
-		
-		for (int i = 0; i < distributionStrings.size(); i++) 
-			stddev += Math.pow((Double.parseDouble(distributionStrings.get(i)) - mean), 2);
-		
-		stddev = Math.sqrt(stddev/(double) distributionStrings.size());
-		
-		// adjusting mean and stddev for degrees
-//		if (mean > 1 && stddev > 1) {
-//			double oldMean = mean;
-//			mean *= this.numSources/(double)38; // 38 sources in real data
-//			stddev *= mean/oldMean;
-//		}
-		
-		double[] values = new double[nObjects];
-		int count = 0;
-
-		// adjusting mean and stddev for degrees
-		if (mean > 1 && stddev > 1) {
-			stddev *= (this.numSources/2)/mean;
-			mean = Math.ceil((double) this.numSources/2);
-		
-			double tempValue;
-			boolean withinRange = false;
-			while (count < nObjects) {
-				withinRange = false;
-				while (!withinRange) {
-					tempValue = Math.abs(random.nextGaussian() * stddev + mean); // degree normal distribution
-					if (tempValue <= this.numSources && tempValue > 1) { //at least two sources and at most n sources vote
-						values[count] = tempValue;
-						count++;
-						withinRange = true;
-					}
-				}
-			}
-		}
-		
-		else {
-			count = 0;
-			while (count < nObjects) {
-				values[count] = Math.abs(random.nextGaussian() * stddev + mean);
-				count++;
-			}
-		}
-		
-		return values;
 	}
 	
 	/*
@@ -136,105 +90,101 @@ public class generator {
 		}
 	}
 	
+	/*
+	 * function to generate completely random data and truth
+	 */
+	public void randomData() {
+		String[] values = {"0", "1"};
+		int randomValue = 0;
+		
+		Random random = new Random(System.currentTimeMillis());
+		for (int i = 0; i < this.numObjects; i++) {
+			for (int j = 0; j < this.numSources; j++) {
+				randomValue = random.nextInt(3);
+				if (randomValue != 2)
+					this.data[i][j] = values[randomValue];
+			}
+			
+			List<String> uniqueValues = this.getObjectUniqueValues(this.data[i]);
+			if (uniqueValues.size() == 0) { // no source reported a value
+				Random r = new Random(System.currentTimeMillis());
+				this.data[i][r.nextInt(this.numSources)] = values[0]; // have a random source report a value
+				
+				this.truth[i][0] = String.valueOf(i);
+				this.truth[i][1] = String.valueOf(values[0]);
+			}
+			
+			else {
+				this.truth[i][0] = String.valueOf(i);
+				this.truth[i][1] = String.valueOf(uniqueValues.get(random.nextInt(uniqueValues.size())));
+			}
+		}
+	}
+	
+	/*
+	 * function to generate data with similar distribution for #votes
+	 */
+	public void degreeData() throws IOException {
+		List<String> oldDegrees = this.getValuesFromFile(this.degreeDistributionFile);
+		
+		double realMean = 0;
+		double realStddev = 0;
+		
+		for (int i = 0; i < oldDegrees.size(); i++) 
+			realMean += Double.parseDouble(oldDegrees.get(i));
+		realMean /= oldDegrees.size();
+		
+		for (int i = 0; i < oldDegrees.size(); i++) 
+			realStddev += Math.pow((Double.parseDouble(oldDegrees.get(i)) - realMean), 2);
+		realStddev = Math.sqrt(realStddev/(double) oldDegrees.size());
+		
+		// adjusting mean and stddev for degrees
+		double newMean = ((double) this.numObjects / oldDegrees.size()) * realMean;
+		double newStddev = ((double) this.numObjects / oldDegrees.size()) * realStddev;
+		
+		String[] values = {"0", "1"};
+		double newDegree;
+		Random r = new Random(System.currentTimeMillis()); // for normal distribution generator
+		for (int i = 0; i < this.numObjects; i++) {
+			newDegree = Math.min(Math.ceil(r.nextGaussian() * newStddev + newMean), this.numSources);
+			for (int j = 0; j < newDegree; j++) {
+				Random r0 = new Random(System.currentTimeMillis()); // for location of vote
+				Random r1 = new Random(System.currentTimeMillis()); // for value of vote
+				int vote = r1.nextInt(3);
+				if (vote != 2)
+					this.data[i][r0.nextInt(this.numSources)] = values[vote];
+			}
+			
+			List<String> uniqueValues = this.getObjectUniqueValues(this.data[i]);
+			if (uniqueValues.size() == 0) { // no source reported a value
+				Random r2 = new Random(System.currentTimeMillis());
+				this.data[i][r2.nextInt(this.numSources)] = values[0]; // have a random source report a value
+				
+				this.truth[i][0] = String.valueOf(i);
+				this.truth[i][1] = String.valueOf(values[0]);
+			}
+			
+			else {
+				Random r3 = new Random(System.currentTimeMillis());
+				this.truth[i][0] = String.valueOf(i);
+				this.truth[i][1] = String.valueOf(uniqueValues.get(r3.nextInt(uniqueValues.size())));
+			}
+		}
+	}
+		
 	/* args[0]: n (#objects)
 	 * args[1]: #sources
 	 * args[2]: degreeFile,
-	 * args[3]: entropyFile
+	 * args[3]: giniFile
+	 * args[4]: fractionVotingFile
+	 * args[5]: conflictRatioFile
 	 */
-	public static void main(String[] args) {
-		int nObjects = Integer.parseInt(args[0]);
-		int nSources = Integer.parseInt(args[1]);
-		
-		generator g = new generator(nObjects, nSources);
-		List<String> uniqueValues = null;
-		
-		if (args.length == 2) { // random data
-			String[] values = {"0", "1"};
-			
-			for (int i = 0; i < g.numObjects; i++) {
-				for (int j = 0; j < g.numSources; j++) {
-					int randomValue = g.random.nextInt(3);
-					if (randomValue != 2)
-						g.data[i][j] = values[randomValue];
-				}
-				uniqueValues = g.getObjectUniqueValues(g.data[i]);
-				if (uniqueValues.size() == 0)
-					g.data[i][0] = values[0];
-				uniqueValues = g.getObjectUniqueValues(g.data[i]);
-				g.truth[i][0] = String.valueOf(i);
-				g.truth[i][1] = String.valueOf(uniqueValues.get(g.random.nextInt(uniqueValues.size()))); 
-			}
-		}
-		
-		if (args.length == 4) { // data follows degree and entropy distribution
-			g.degreeDistributionFile = args[2];
-			g.entropyDistributionFile = args[3];
-			
-			List<String> degreeStrings = new ArrayList<String>();
-			List<String> entropyStrings = new ArrayList<String>();
-			
-			try {
-				degreeStrings = g.getValuesFromFile(args[2]);
-			} catch (IOException e) {
-				System.out.println("Reading error: degree file");
-			}
-			
-			try {
-				entropyStrings = g.getValuesFromFile(args[3]);
-				entropyStrings.removeAll(Collections.singleton("0.0"));
-			} catch (IOException e) {
-				System.out.println("Reading error: entropy file");
-			} 
-			
-			double[] degreeOfObjects = g.drawFromNormalDistribution(degreeStrings, g.numObjects);
-			for (int i = 0; i < degreeOfObjects.length; i++) 
-				degreeOfObjects[i] = Math.ceil(degreeOfObjects[i]);
-			
-			// barring single-valued objects, entropy follows a normal distribution
-			double[] entropyOfObjects = g.drawFromNormalDistribution(entropyStrings, g.numObjects);
-			
-			int countDegree = 0;
-			int countEntropy = 0;
-			double differentVotes = 0;
-			String[] values = {"0", "1"};
-			
-			for (int i = 0; i < g.numObjects; i++) {
-				boolean[] votedBy = new boolean[g.numSources];
-				countDegree = 0;
-				countEntropy = 0;
-				System.out.println(i + ":\t" + degreeOfObjects[i] + "\t" + entropyOfObjects[i]);
-				if (entropyOfObjects[i] <= 0.5) { // real roots
-					differentVotes = Math.ceil((1 - Math.sqrt(1 - 2 * entropyOfObjects[i]))/2 * degreeOfObjects[i]);
-					if(Double.isNaN(differentVotes))
-						differentVotes = Math.ceil((1 + Math.sqrt(1 - 2 * entropyOfObjects[i]))/2 * degreeOfObjects[i]);
-				}
-				else // imaginary roots
-					differentVotes = Math.ceil(degreeOfObjects[i]/2);
-				
-				// take degree distribution into account 
-				while (countDegree < degreeOfObjects[i]) {
-					int randomSource = g.random.nextInt(g.numSources);
-					if (!votedBy[randomSource]) {
-						votedBy[randomSource] = true;
-						countDegree++;
-						// take entropy distribution into account
-						if (countEntropy < differentVotes) {
-							g.data[i][randomSource] = String.valueOf(1);
-							countEntropy++;
-						}
-						else 
-							g.data[i][randomSource] = String.valueOf(0);
-				 	}
-				}
-				uniqueValues = g.getObjectUniqueValues(g.data[i]);
-				if (uniqueValues.size() == 0)
-					g.data[i][0] = values[0];
-				
-				g.truth[i][0] = String.valueOf(i);
-				g.truth[i][1] = String.valueOf(uniqueValues.get(g.random.nextInt(uniqueValues.size())));
-			}
-		}
-		
+	public static void main(String[] args) throws IOException {
+		generator g = new generator(args);
+		g.randomData();
+		g.generateDataAndTruthFiles();
+
+		g.degreeData();
 		g.generateDataAndTruthFiles();
 
 		
